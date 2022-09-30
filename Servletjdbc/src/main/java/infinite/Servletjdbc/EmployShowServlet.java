c5f53c20ec976aefef8357c7ad596144550cdfbb
gummabhargavi <108506501+gummabhargavi@users.noreply.github.com>
2022-09-30 15:34:41 +0530

    Add files via upload

---
+++ Servletjdbc/src/main/java/infinite/Servletjdbc/EmployShowServlet.java
@@ -0,0 +1,55 @@
+package infinite.Servletjdbc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class EmployShowServlet
+ */
+public class EmployShowServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EmployShowServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		EmployDAO dao = new EmployDAO();
+		try {
+			List<Employ> employList = dao.showEmploy();
+			for (Employ employ : employList) {
+				out.println("Employ No " +employ.getEmpno() + "<br/>");
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
